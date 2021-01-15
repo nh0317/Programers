5cc0890b334722249b18fd4092252de1c00c9918
KNH <nh2004@naver.com>
2021-01-16 01:08:32 +0900

    level1 문자열을 정수로 바꾸기
    java 풀이 및 정리

---
+++ 연습문제/문자열을 정수로 바꾸기/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer=Integer.parseInt(s);
+        return answer;
+    }
+}
+
+/*
+COMMENT
+문자열 s를 숫자로 변환하는 문제이다. 
+parseInt 메소드를 이용하면 간단하게 풀 수 있다. 
+*/
