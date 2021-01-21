2e789753ae3839d25a6a34850fdf0e3b6b6053f0
KNH <nh2004@naver.com>
2021-01-21 23:33:28 +0900

    level1 짝수와 홀수
    짝수와 홀수를 구분하는 문제
    java 풀이 및 정리

---
+++ 연습문제/짝수와 홀수/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2==0) answer="Even";
+        else answer="Odd";
+        return answer;
+    }
+}
+
+/*
+COMMENT
+짝수를 판별하는 문제 
+조건문을 통해 간단하게 구현할 수 있다. 
+*/
